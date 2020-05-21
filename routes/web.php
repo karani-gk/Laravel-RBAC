6e00012cb01b4ab146b6037e9d44471ce20c4163
Karani G.K <geoffrey.kamundi@gmail.com>
2020-05-21 14:59:35 +0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('products','ProductController');
+});
